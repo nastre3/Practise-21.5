8bb8fea62e757c5f265e11cb36a5d5b3f5c400e4
nastre3 <nastre70@gmail.com>
2021-11-02 19:49:19 +1000

    Fixed Task4

--- 1/src/BookMarket/Main.java
+++ 1/src/BookMarket/Main.java
@@ -11,26 +11,54 @@ public class Main {
         book2.name = "Стихи";
         book2.price = 200;
         book2.publishedDate = 2000;
-        Book[] bookLibrary = {book1, book2};
-        findBook("Стихи", 2000, bookLibrary);
+
+        Book book3 = new Book();
+        book3.name = "Стихи2";
+        book3.price = 100;
+        book3.publishedDate = 1800;
+
+        Book[] bookLibrary = new Book[2];
+        System.out.println(bookLibrary.length);
+        addBook(bookLibrary, book1);
+        addBook(bookLibrary, book2);
+        addBook(bookLibrary, book3);
+
+        isBookInLibrary(bookLibrary, book1);
         printLibrary(bookLibrary);
     }
 
-    private static void printLibrary(Book[] bookLibrary) {
-        for (Book i : bookLibrary
-        ) {
-            System.out.printf("книга №: название: %s, год издания: %dг., цена - %dusd \n", i.name, i.publishedDate, i.price);
+    private static void addBook(Book[] bookLibrary, Book book) {
+        if (!isBookInLibrary(bookLibrary, book)) {
+            for (int i = 0; i < bookLibrary.length; i++) {
+                if (bookLibrary[i] == null) {
+                    bookLibrary[i] = book;
+                    System.out.println("Книга добавлена в картотеку");
+                    break;
+                }
+                if (i == bookLibrary.length - 1) { // тк индекс с 0
+                    System.out.println("Картотека переполнена");
+                }
+            }
         }
     }
 
-    private static void findBook(String name, int publishedDate, Book[] bookLibrary) {
-        for (Book i:bookLibrary
-             ) {
-            if (name.equals(i.name)) {
-                System.out.println("Книга есть в картотеке");
-            } else {
-                System.out.println("Книгу добавили в картотеку");
+    private static void printLibrary(Book[] bookLibrary) {
+        for (int i = 0; i < bookLibrary.length; i++) {
+            if (bookLibrary[i] != null) {
+                System.out.printf("книга №%d: название: %s, год издания: %dг., цена - %dusd \n", i + 1, bookLibrary[i].name, bookLibrary[i].publishedDate, bookLibrary[i].price);
             }
         }
+    }
+
+    protected static boolean isBookInLibrary(Book[] bookLibrary, Book book) {
+        for (Book i : bookLibrary
+        ) {
+            if (i != null && book.name.equals(i.name) && book.publishedDate == i.publishedDate) {
+                System.out.println("Книга есть в картотеке");
+                return true;
+            }
+        }
+        System.out.println("Книги нет в картотеке");
+        return false;
     }
 }
